3ba7c5dda1a9acab44405265e550e78d2e019b1a
DanteBenicio <dantecosta79@gmail.com>
2022-03-16 19:15:09 -0300

    showOptions feature

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,4 +1,3 @@
-import { MouseEvent, useRef } from 'react';
 import { ArrowDark } from '../../svgs/icon-arrow-dark';
 import Button from '../Button';
 import './styles.scss';
@@ -10,6 +9,24 @@ type SidebarProps = {
 
 export default function Sidebar({ burger, toggleBurgerValue }: SidebarProps) {
   return (
+  const listRef = useRef<HTMLUListElement | null>(null)
+
+  function showOptions(e: MouseEvent) {
+    const list = Array.from(listRef.current?.children!)
+    const liClicked = e.currentTarget;
+
+    list.forEach(li => {
+      if (li === liClicked) {
+        liClicked.classList.toggle('active');
+        return
+      }
+
+      if (li.classList.contains('active')) {
+        li.classList.remove('active')
+      }
+    })
+  }
+
     <div className="sidebar_container">
       <div className="sidebar_wrapper">
         <div className="buttons_container">
