e7fd6030236853bca51fbdb82a6076e448a95d01
DanteBenicio <dantecosta79@gmail.com>
2022-03-16 19:17:29 -0300

    options container

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,5 @@
+import { MouseEvent, useRef } from 'react';
 import { ArrowDark } from '../../svgs/icon-arrow-dark';
-import Button from '../Button';
 import './styles.scss';
 
 type SidebarProps = {
@@ -8,6 +8,24 @@ type SidebarProps = {
 }
 
 export default function Sidebar({ burger, toggleBurgerValue }: SidebarProps) {
+  const listRef = useRef<HTMLUListElement | null>(null)
+
+  function showOptions(e: MouseEvent) {
+    const list = Array.from(listRef.current?.children!)
+    const liClicked = e.currentTarget;
+
+    list.forEach(li => {
+      if (li === liClicked) {
+        liClicked.classList.toggle('active');
+        return
+      }
+
+      if (li.classList.contains('active')) {
+        li.classList.remove('active')
+      }
+    })
+  }
+
   return (
     <div className="sidebar_container">
       <div className="sidebar_wrapper">
@@ -22,21 +40,31 @@ export default function Sidebar({ burger, toggleBurgerValue }: SidebarProps) {
           </div>
         </div>
 
-        <ul className="sidebar_list">
-          <li className="select">
+        <ul className="sidebar_list" ref={listRef!}>
+          <li className="select" onClick={e => showOptions(e)}>
             <span>Product <ArrowDark /></span>
-            {/* <div>
-              <p></p>
-              <p></p>
-              <p></p>
-            </div> */}
           </li>
-          <li className="select">
+          <div className="options_container">
+            <p>Contact</p>
+            <p>Newsletter</p>
+            <p>LinkedIn</p>
+          </div>
+          <li className="select" onClick={e => showOptions(e)}>
             <span>Company <ArrowDark /></span>
           </li>
-          <li className="select">
+          <div className="options_container">
+            <p>Contact</p>
+            <p>Newsletter</p>
+            <p>LinkedIn</p>
+          </div>
+          <li className="select" onClick={e => showOptions(e)}>
             <span>Connect <ArrowDark /></span>
           </li>
+          <div className="options_container">
+            <p>Contact</p>
+            <p>Newsletter</p>
+            <p>LinkedIn</p>
+          </div>
         </ul>
       </div>
     </div>
