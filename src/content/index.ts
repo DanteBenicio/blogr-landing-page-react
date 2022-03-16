8bd57a72355d2a34524da5b2451a5cdc8fb9661f
DanteBenicio <dantecosta79@gmail.com>
2022-03-15 23:23:51 -0300

    texts application

---
+++ src/content/index.ts
@@ -0,0 +1,37 @@
+export const content1 = {
+  title: 'Designed for the future',
+  subTitle: 'Introducing an extensible editor',
+  secondarySubTitle: 'Robust content management',
+  firstDescription: `
+    Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content.
+    The editor supports management of multiple blogs and allows easy manipulation of embeds such as images,
+    videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functinoality or change
+    the looks of a blog.
+  `,
+  secondaryDescription: `
+    Flexible content management enables users to easily move through posts. 
+    Increase the usability of your blog by adding customized categories, sections, 
+    format, or flow. With this functionality, you’re in full control.
+  `,
+  desktopImgSrc: 'assets/illustration-editor-desktop.svg',
+  mobileImgSrc: 'assets/illustration-editor-mobile.svg',
+  rowReverse: false
+}
+
+export const content2 = {
+  subTitle: 'Free, open, simple',
+  secondarySubTitle: 'Powerful tooling',
+  firstDescription: `
+    Blogr is a free and open source application backed by a large community of helpful developers. 
+    It supports features such as code syntax highlighting, RSS feeds, social media integration, 
+    third-party commenting tools, and works seamlessly with Google Analytics. 
+    The architecture is clean and is relatively easy to learn. 
+  `,
+  secondaryDescription: `
+    Batteries included. We built a simple and straightforward CLI tool that makes customization and deployment a breeze, 
+    but capable of producing even the most complicated sites.
+  `,
+  desktopImgSrc: 'assets/illustration-laptop-desktop.svg',
+  mobileImgSrc: 'assets/illustration-laptop-mobile.svg',
+  rowReverse: true
+}
