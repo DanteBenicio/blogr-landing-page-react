e67ec7b4239db7e1c665487e80874520afde90e1
DanteBenicio <dantecosta79@gmail.com>
2022-03-15 23:20:53 -0300

    MidSection component

---
+++ src/components/MidSection/index.tsx
@@ -0,0 +1,29 @@
+import './styles.scss';
+
+export default function MidSection() {
+  return (
+    <section className="mid_section_container">
+      <main className="mid_section_wrapper">
+        <div className="images_wrapper">
+          <img 
+            src="assets/illustration-phones.svg" 
+            alt="phones image"
+            className="phones_image"
+          />
+          <img 
+            src="assets/bg-pattern-circles.svg" 
+            alt="phones image"
+            className="purple_circle"
+          />
+        </div>
+        <div className="content_wrapper">
+          <h2 className="mid_section_title">State of the Art Infrastructure</h2>
+          <p>
+            With reliability and speed in mind, worldwide data centers provide the backbone for ultra-fast connectivity. 
+            This ensures your site will load instantly, no matter where your readers are, keeping your site competitive.
+          </p>
+        </div>
+      </main>
+    </section>
+  )
+}
