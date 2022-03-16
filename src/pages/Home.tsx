c34b04187f1919b2988934f0796beb924ca37402
DanteBenicio <dantecosta79@gmail.com>
2022-03-16 19:10:29 -0300

    pages folder

---
+++ src/pages/Home.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import Footer from '../components/Footer'
+import MidSection from '../components/MidSection'
+import Navbar from '../components/Navbar'
+import Section from '../components/Section'
+import SectionHero from '../components/SectionHero'
+import Sidebar from '../components/Sidebar'
+import { content1, content2 } from '../content'
+
+export default function Home() {
+  const [burger, setBurger] = useState<boolean>(false);
+
+  const toggleBurgerValue = () => setBurger(prevBurgerValue => !prevBurgerValue);
+  
+  return (
+    <>
+      {burger && <Sidebar burger={burger} toggleBurgerValue={toggleBurgerValue}/>}
+      <Navbar burger={burger} toggleBurgerValue={toggleBurgerValue}/>
+      <SectionHero />
+      <Section {...content1}/>
+      <MidSection />
+      <Section {...content2}/>
+      <Footer />
+    </>
+  )
+}
