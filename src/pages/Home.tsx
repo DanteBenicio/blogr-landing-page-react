74039f97f3a8e704a335ef85b54bfc3b300f40eb
DanteBenicio <dantecosta79@gmail.com>
2022-03-18 14:15:51 -0300

    eventListener on window object

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,7 +11,9 @@ export default function Home() {
   const [burger, setBurger] = useState<boolean>(false);
 
   const toggleBurgerValue = () => setBurger(prevBurgerValue => !prevBurgerValue);
-  
+
+  window.addEventListener('scroll', () => showScrollTopButton(scrollTopElementRef.current!))
+    
   return (
     <>
       {burger && <Sidebar burger={burger} toggleBurgerValue={toggleBurgerValue}/>}
