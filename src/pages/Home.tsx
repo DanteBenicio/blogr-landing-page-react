7d4ce3176c8ee301fa78b869e775a7d2fa3faf39
DanteBenicio <dantecosta79@gmail.com>
2022-03-18 14:17:20 -0300

    import useRef hook

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import Footer from '../components/Footer'
 import MidSection from '../components/MidSection'
 import Navbar from '../components/Navbar'
